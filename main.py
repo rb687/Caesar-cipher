6f1324b991408826080a5cd2b33063c2dfde81e3
Richa Bhartia <richabhartia@Richas-MacBook-Pro.local>
2016-12-17 22:22:39 -0500

    Checking git integration

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+alphabet_list = ['A', 'B', 'C', 'D', 'E',
+                 'F', 'G', 'H', 'I', 'J',
+                 'K', 'L', 'M', 'N', 'O',
+                 'P', 'Q', 'R', 'S', 'T',
+                 'U', 'V', 'W', 'X', 'Y',
+                 'Z' ]
+
+
+def calc_shift(shiftBy):
+    if shiftBy >= 0:
+        i = shiftBy
+
+
+
+def encrypt(message):
